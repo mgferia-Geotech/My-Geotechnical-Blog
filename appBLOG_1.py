fa14a9dfca79e1f90a3f8557cd14c1a0716c26f1
mgferia-Geotech <mgferia@gmail.com>
2026-01-21 19:28:33 -0500

    Update appBLOG_1.py

--- appBLOG_1.py
+++ appBLOG_1.py
@@ -471,8 +471,8 @@ s2, fs2, pf2, m_fs2, l2, h2, w2 = calculate_data(55, cov2, Ns2, H, gamma, SAMPLE
 fig = make_subplots(
     rows=2, cols=2,
     subplot_titles=(
-        f"Escenario 1 Su - CoV: {cov1:.0%}", 
-        f"Escenario 2 Su - CoV: {cov2:.0%}",
+        f" ", #f"Escenario 1 Su - CoV: {cov1:.0%}"
+        f" ", #f"Escenario 2 Su - CoV: {cov2:.0%}"
         f"Escenario 1 - Pf: {pf1:.2%}", 
         f"Escenario 2 - Pf: {pf2:.2%}"
     ),
@@ -513,7 +513,7 @@ fig.add_trace(go.Scatter(x=[m_fs2], y=[kde2(m_fs2)[0]*SAMPLES*0.05], mode='marke
 # --- CONFIGURACIÓN FINAL ---
 fig.update_layout(height=350, template="plotly_white", barmode='overlay', showlegend=False, margin=dict(t=0, b=20, l=10, r=10), dragmode=False)
 fig.update_xaxes(title_text="Su (kPa)", range=[0, 130], row=1)
-fig.update_xaxes(title_text="Factor de Seguridad (FS)", range=[0.0, 3.0], row=2)
+fig.update_xaxes(title_text="Factor de Seguridad (FS)\n\n Escenario 1", range=[0.0, 3.0], row=2)
 fig.update_yaxes(title_text=" ", row=1, range=[0, 1500])
 fig.update_yaxes(title_text=" ", row=2, range=[0, 1500])
 fig.add_vline(x=1.0, line_dash="dash", line_color="grey", row=2, col=1, line_width=0.5)
@@ -523,7 +523,7 @@ fig.add_vline(x=1.0, line_dash="dash", line_color="grey", row=2, col=2, line_wid
 
 st.plotly_chart(fig, use_container_width=True, config=plotly_config)
 
-st.warning("En términos estadisticos diríamos que el talud del **Escenario 1 es casi 23 veces más propenso a fallar que el talud del Escenario 2**.", icon="⚠️")
+st.warning("En términos estadisticos diríamos que el talud del **Escenario 1 es 20 o más veces más propenso a fallar que el talud del Escenario 2**.", icon="⚠️")
   
 
 st.divider() # Visual line separator
@@ -544,6 +544,7 @@ pero potencialmente engañoso. En geotecnia no buscamos la certeza absoluta, sin
 """)
 
 st.success('**Próxima entrega: *Estadística bayesiana* o porque los geotecnista somos *bayesianos* innatos**', icon="✍🏼")
+
 
 
 
